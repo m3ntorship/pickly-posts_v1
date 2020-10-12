ef54d26902fbe743cad81f627ffc7f9b2bf7c16f
Ahmed Ayoub <great.nile@gmail.com>
2020-10-12 18:46:02 +0200

    small refactoring

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,8 @@
 const appError = require("../util/appError");
 const activeEmailsModel = require("../models/activeEmailsModel");
-const {verifyIdToken} = require('../util/verifyIdToken');
+const { verifyIdToken } = require("../util/verifyIdToken");
 
-
-module.exports = (req, res, next) => {
+module.exports.protector = (req, res, next) => {
   let {
     headers: { authorization },
   } = req;
@@ -12,15 +11,8 @@ module.exports = (req, res, next) => {
     let [, token] = authorization.split(/bearer /i);
     verifyIdToken(token)
       .then((tokeninfo) => {
-        return activeEmailsModel.findOne({ email: tokeninfo.email });
-      })
-
-      .then((isActive) => {
-        if (isActive) {
-          return next();
-        } else {
-          return next(new Error(403));
-        }
+        req.user = tokeninfo;
+        next();
       })
       .catch((error) => {
         next(new appError("unauthorized", 401));
@@ -28,4 +20,14 @@ module.exports = (req, res, next) => {
   } else {
     next(new appError("unauthorized", 401));
   }
+};
+
+module.exports.activeUsersOnly = (req, res, next) => {
+  activeEmailsModel.findOne({ email: req.user.email }).then((isActive) => {
+    if (isActive) {
+      return next();
+    } else {
+      return next(new Error(403));
+    }
+  });
 };
