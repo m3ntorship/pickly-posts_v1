4296ebc6099b0a7f85a0ff50fa67419d1a6957a2
Abud Zohair <abud.zohair@gmail.com>
2020-10-17 10:57:23 +0200

    Edit upvoting handler
    - make the upvoting handler compatible with the votes model

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,10 +1,11 @@
-const multer = require("multer");
+const multer = require('multer');
 
-const { Image } = require("../models/imageModel");
-const factory = require("./handlerFactory");
-const catchAsync = require("./../util/catchAsync");
-const AppError = require("../util/appError");
-const User = require("../models/userMode");
+const { Image } = require('../models/imageModel');
+const factory = require('./handlerFactory');
+const catchAsync = require('./../util/catchAsync');
+const AppError = require('../util/appError');
+const User = require('../models/userMode');
+const Votes = require('../models/votesModel');
 
 exports.getImage = async (req, res, next) => {
   const { id } = req.params;
@@ -14,10 +15,10 @@ exports.getImage = async (req, res, next) => {
 };
 exports.uploadImage = (req, res) => {
   const upload = multer({
-    storage: cloudinaryStorage,
-  }).array("image", 2);
+    storage: cloudinaryStorage
+  }).array('image', 2);
 
-  upload(req, res, (err) => {
+  upload(req, res, err => {
     if (err) {
       res.send(err);
     }
@@ -33,22 +34,20 @@ exports.upvote = catchAsync(async (req, res, next) => {
     params: { imageId },
     user,
     user: {
-      mongouser: { _id: userId },
-    },
+      mongouser: { _id: userId }
+    }
   } = req;
-
-  
 
   if (!user) return next(new AppError(`User isn't Found`, 401));
 
-  const image = await Image.findById(imageId);
-  if (!image) return next(new AppError("Image is not found", 404));
-  if (!image.votes.find((vote) => vote.toString() === userId.toString())) {
-    image.votes.push(userId.toString()  );
-    image.voutesCount = image.voutesCount + 1; // TODO: move this to separate vote model
-    await image.save(); //TODO: handle this error properly
-    res.json({ image });
+  const imageVotes = await Votes.findOne({ image: imageId });
+  if (!imageVotes) return next(new AppError('Image  not found', 404));
+  if (!imageVotes.votedBy.find(user => user.toString() === userId.toString())) {
+    imageVotes.votedBy.push(userId.toString());
+    imageVotes.totalVotes += 1;
+    await imageVotes.save();
+    return res.json({ votes: imageVotes.totalVotes });
   } else {
-    return next(new AppError("Already Voted", 400));
+    return next(new AppError('Already Voted', 400));
   }
 });
