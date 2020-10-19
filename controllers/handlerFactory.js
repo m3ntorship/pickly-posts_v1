8b8dfddba54c6fef2adbb26235865aa8014280e1
Abud Zohair <abud.zohair@gmail.com>
2020-10-19 14:22:43 +0200

    send the data via toJSONFor method
    - return voted propery with boolean value to be checked form front-end
    - reduce queries to DB
    - link resouces to imageId only
    - populate author if the post is not Anonymous only

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -2,109 +2,123 @@ const catchAsync = require('../util/catchAsync');
 const AppError = require('../util/appError');
 const { Image } = require('../models/imageModel');
 const { Resources } = require('../models/imageModel');
-const User = require('../models/userMode');
 const Post = require('../models/postModel');
-const { Model } = require('mongoose');
 const isTruthy = require('../util/isTruthy');
-exports.createOne = (Model) =>
-	catchAsync(async (req, res, next) => {
-		if (!req.files.images) {
-			return next(new AppError('Please Upload atleast one image', 400));
-		}
-		const images = req.files.images.map((image) => {
-			return {
-				name: image.originalname.replace(' ', ''),
-				url: image.path,
-				provider: 'cloudinary',
-			};
-		});
-		const image = await Image.create(images);
-		const resources = await Resources.create({ images: image });
-		const { isAnonymous, caption } = req.body;
-		const isAnonymousBoolean = isTruthy(isAnonymous);
-		const user = req.user.mongouser;
 
-		const doc = await Model.create({
-			caption,
-			resources: resources._id,
-			author: user._id,
-			isAnonymous: isAnonymousBoolean,
-		});
-		resources['images'].forEach(async (img) => {
-			const image = await Image.findByIdAndUpdate(
-				img,
-				{
-					linkedPost: doc._id,
-				},
-				{ new: true }
-			).exec();
-		});
-		res.status(201).json({
-			status: 'success',
-			data: doc,
-		});
-	});
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    if (!req.files.images) {
+      return next(new AppError('Please Upload atleast one image', 400));
+    }
+    const images = req.files.images.map(image => {
+      return new Image({
+        name: image.originalname.replace(' ', ''),
+        url: image.path,
+        provider: 'cloudinary'
+      });
+    });
+
+    const imagesIds = images.map(image => image._id);
+
+    const resources = await Resources.create({ images: imagesIds });
+    const { isAnonymous, caption } = req.body;
+    const isAnonymousBoolean = isTruthy(isAnonymous);
+    const user = req.user.mongouser;
+
+    const doc = await Model.create({
+      caption,
+      resources: resources._id,
+      author: user._id,
+      isAnonymous: isAnonymousBoolean
+    });
+
+    images.forEach(async img => {
+      img.postId = doc._id;
+      await img.save();
+    });
+
+    res.status(201).json({
+      status: 'success',
+      data: doc.toJSONFor(req.user.mongouser)
+    });
+  });
 
 exports.getOne = (Model, popOptions) =>
-	catchAsync(async (req, res, next) => {
-		let query = Model.findById(req.params.id);
-		if (popOptions)
-			query = query.populate(popOptions).populate('author').exec();
-		const doc = await query;
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions)
+      query = query
+        .populate({
+          path: popOptions,
+          model: 'resources',
+          populate: {
+            path: 'images',
+            model: 'image'
+          }
+        })
+        .exec();
+    const doc = await query;
 
-		if (!doc) {
-			return next(new AppError('No document found with that ID', 404));
-		}
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
 
-		res.status(200).json({
-			status: 'success',
-			data: doc,
-		});
-	});
+    if (doc.author) {
+      await doc.populate('author', 'name email').execPopulate();
+    }
 
-exports.updateOne = (Model) =>
-	catchAsync(async (req, res, next) => {
-		const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-			runValidators: true,
-		});
+    res.status(200).json({
+      status: 'success',
+      data: doc.toJSONFor(req.user.mongouser)
+    });
+  });
 
-		if (!doc) {
-			return next(new AppError('No document found with that ID', 404));
-		}
-		res.status(200).json({
-			status: 'success',
-			data: {
-				data: doc,
-			},
-		});
-	});
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
 
-exports.deleteOne = (Model) =>
-	catchAsync(async (req, res, next) => {
-		const docId = req.params.id;
-		const doc = await Model.deleteOne({ _id: docId });
-		console.log(doc);
-		if (doc) res.status(204).send();
-		return next(new AppError('cannot find doc with that id', 404));
-	});
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc.toJSONFor(req.user.mongouser)
+      }
+    });
+  });
+
+exports.deleteOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const docId = req.params.id;
+    const doc = await Model.deleteOne({ _id: docId });
+    console.log(doc);
+    if (doc) res.status(204).send();
+    return next(new AppError('cannot find doc with that id', 404));
+  });
 
 exports.getAll = (Model, options) =>
-	catchAsync(async (req, res, next) => {
-		let data = Post.find();
-		if (options.getRecentFirst) {
-			data.sort('-createdAt');
-		}
-		if (options.populateResources) {
-			data.populate('resources');
-		}
-		if (options.populateAuthor) {
-			data.populate('author');
-		}
-		data = await data;
-		if (!data) {
-			return next(new AppError('No Polls found with that ID', 404));
-		} else {
-			res.status(200).json({ data });
-		}
-	});
+  catchAsync(async (req, res, next) => {
+    let data = Post.find();
+    if (options.getRecentFirst) {
+      data.sort('-createdAt');
+    }
+    if (options.populateResources) {
+      data.populate('resources');
+    }
+    if (options.populateAuthor) {
+      data.populate('author');
+    }
+    data = await data;
+
+    if (!data) {
+      return next(new AppError('No Polls found with that ID', 404));
+    } else {
+      res
+        .status(200)
+        .json({ data: data.map(post => post.toJSONFor(req.user.mongouser)) });
+    }
+  });
