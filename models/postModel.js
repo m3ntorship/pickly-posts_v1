d829f5101bee102b36a31f272724e2ded92b5012
Abud Zohair <abud.zohair@gmail.com>
2020-10-19 14:12:59 +0200

    Add toJSONFor method
    - toJSONFor method => returns only fields we slect and decide if the post is anon or is voted by the user

--- models/postModel.js
+++ models/postModel.js
@@ -1,32 +1,36 @@
 const mongoose = require('mongoose');
-const isTruthy = require('../util/isTruthy');
 
 const postSchema = new mongoose.Schema(
-	{
-		caption: {
-			type: String,
-			required: true,
-		},
-		resources: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'resources',
-			required: true,
-		},
-		isAnonymous: {
-			type: Boolean,
-			default: false,
-		},
-		author: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
-	},
-	{
-		toJSON: {
-			transform(doc, ret) {
-				if (ret.isAnonymous) ret.author = undefined;
-			},
-		},
-		timestamps: true,
-	}
+  {
+    caption: {
+      type: String,
+      required: true
+    },
+    resources: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'resources',
+      required: true
+    },
+    isAnonymous: {
+      type: Boolean,
+      default: false
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'user' }
+  },
+
+  {
+    timestamps: true
+  }
 );
 
+postSchema.methods.toJSONFor = function (user) {
+  return {
+    caption: this.caption,
+    resources: this.resources,
+    isAnonymous: this.isAnonymous,
+    author: this.isAnonymous ? undefined : this.author,
+    Voted: user ? user.isVoted(this._id) : false
+  };
+};
 const Post = mongoose.model('Post', postSchema);
 module.exports = Post;
