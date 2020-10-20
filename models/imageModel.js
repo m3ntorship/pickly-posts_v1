1751769894388777b49432479b4d663fae0bb89a
Abud Zohair <abud.zohair@gmail.com>
2020-10-20 16:18:53 +0200

    Delete the pre save hook

--- models/imageModel.js
+++ models/imageModel.js
@@ -1,25 +1,20 @@
 const mongoose = require('mongoose');
 const Votes = require('./votesModel');
 
-const imageSchema = new mongoose.Schema({
-  name: String,
-  url: String,
-  provider: String,
+const imageSchema = new mongoose.Schema(
+  {
+    name: String,
+    url: String,
+    provider: String,
 
-  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
-  votes: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Votes'
-  }
-});
-
-imageSchema.pre('save', async function () {
-  const votes = await Votes.create({
-    image: this._id,
-    postId: this.postId.toString()
-  });
-  this.set('votes', votes._id);
-});
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
+    votes: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Votes'
+    }
+  },
+  { versionKey: false }
+);
 
 const Image = mongoose.model('image', imageSchema);
 
