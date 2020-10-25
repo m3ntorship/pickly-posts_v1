7772c2b633a945591b6025f1b1bc89a6c2da6fd9
Ahmed Ayoub <great.nile@gmail.com>
2020-10-25 19:25:14 +0100

    separate protector into its own package @m3ntorship/pickly-protector

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const postRouter = require('./posts/post.routes');
 const imageRouter = require('./images/image.routes');
 const { protector } = require('./auth/auth.controller');
 const errorHandler = require('./middleware/errorhandler');
-
 dotenv.config({ path: resolve('secrets', '.env') });
 
 const app = express();
@@ -14,14 +13,14 @@ const app = express();
 app.use(cors(), express.json());
 
 app.get('/health', (req, res) => {
-	res.json({ ok: true });
+  res.json({ ok: true });
 });
 
 // protect all routes
 app.use(protector);
 
 app.get('/protected', (req, res) => {
-	res.json({ protected: true });
+  res.json({ protected: true });
 });
 
 app.use('/posts', postRouter);
--- auth/auth.controller.js
+++ auth/auth.controller.js
@@ -1,40 +1,19 @@
 const appError = require('../util/appError');
-const { verifyIdToken } = require('../util/verifyIdToken');
+const admin = require('firebase-admin');
+const { resolve } = require('path');
 const User = require('./user.model');
 const catchAsync = require('../util/catchAsync');
+const { protector } = require('@m3ntorship/pickly-protector');
+const serviceAccount = resolve('secrets', 'service-account.json');
 
-module.exports.protector = catchAsync(async (req, res, next) => {
-  let {
-    headers: { authorization }
-  } = req;
-  if (!authorization)
-    return next(
-      new appError('please provide valid user token to proceed', 401)
-    );
-  const [, token] = authorization.split(/bearer /i);
-  const tokeninfo = await verifyIdToken(token);
-
-  req.user = { tokeninfo };
-
-  let mongoUser = await User.findOne({ email: req.user.tokeninfo.email });
+const userEnricher = async user => {
+  let mongoUser = await User.findOne({ email: user.tokeninfo.email });
   if (!mongoUser)
     mongoUser = await User.create({
-      name: req.user.tokeninfo.name,
-      email: req.user.tokeninfo.email
+      name: user.tokeninfo.name,
+      email: user.tokeninfo.email
     });
-  req.user.mongouser = mongoUser;
-
-  next();
-});
-
-module.exports.activeUsersOnly = (req, res, next) => {
-  activeEmailsModel
-    .findOne({ email: req.user.tokeninfo.email })
-    .then(isActive => {
-      if (isActive) {
-        return next();
-      } else {
-        return next(new Error(403));
-      }
-    });
+  return mongoUser;
 };
+
+module.exports.protector = catchAsync(protector(serviceAccount, userEnricher));
