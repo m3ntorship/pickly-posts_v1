2ba45e51cdaba140a3ae2b9b72fad96b7c293cd5
Abdalla Elmahdy <52469229+abdalla-elmahdy@users.noreply.github.com>
2020-10-29 18:33:54 +0200

    Added basic logging functionality - added winston and express winston packages to the application - added basic logging functionality to the console for requests and errors

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 const express = require('express');
 const { resolve } = require('path');
 const cors = require('cors');
+const winston = require('winston');
+const expressWinston = require('express-winston');
 const postRouter = require('./posts/post.routes');
 const imageRouter = require('./images/image.routes');
 const { protector } = require('./auth/auth.controller');
@@ -11,6 +13,19 @@ dotenv.config({ path: resolve('secrets', '.env') });
 const app = express();
 
 app.use(cors(), express.json());
+
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: false,
+    msg: 'HTTP {{req.method}} {{req.url}}',
+    expressFormat: true
+  })
+);
 
 app.get('/health', (req, res) => {
   res.json({ ok: true });
@@ -25,6 +40,16 @@ app.get('/protected', (req, res) => {
 
 app.use('/posts', postRouter);
 app.use('/images', imageRouter);
+
+app.use(
+  expressWinston.errorLogger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    )
+  })
+);
 
 app.use(errorHandler);
 
