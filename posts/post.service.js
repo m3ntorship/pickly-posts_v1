a405f8795ed871003041df18e4f3d4160720b105
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-10-31 10:54:56 +0200

    unselected unnecessary properties - renamed a function

--- posts/post.service.js
+++ posts/post.service.js
@@ -37,160 +37,201 @@ exports.postService = {
       req.user.mongouser.posts.push(doc._id);
       await req.user.mongouser.save();
 
-			images.forEach(async (img) => {
-				img.postId = doc._id;
-				await img.save();
-			});
+      images.forEach(async img => {
+        img.postId = doc._id;
+        await img.save();
+      });
 
-			res.status(201).json({
-				status: 'success',
-				data: doc.toJSONFor(req.user.mongouser),
-			});
-		});
-	},
-	get(popOptions) {
-		return catchAsync(async (req, res, next) => {
-			let query = Post.findById(req.params.id);
-			let doc = await query;
+      res.status(201).json({
+        status: 'success',
+        data: doc.toJSONFor(req.user.mongouser)
+      });
+    });
+  },
+  get(popOptions) {
+    return catchAsync(async (req, res, next) => {
+      let query = Post.findById(req.params.id);
 
-			if (!doc) {
-				return next(
-					new AppError('No document found with that ID', 404)
-				);
-			}
+      const votedImage = await Votes.findOne({
+        postId: (await query)._id,
+        voters: { $in: req.user.mongouser._id }
+      }).select('image');
+      let doc = await query;
 
-			if (doc.author) {
-				await doc.populate('author', 'name email').execPopulate();
-			}
+      if (!doc) {
+        return next(new AppError('No document found with that ID', 404));
+      }
 
-			if (req.user.mongouser.isVoted(doc._id)) {
-				await doc
-					.populate({
-						path: popOptions,
+      if (doc.author) {
+        await doc.populate('author', 'name email').execPopulate();
+      }
+
+      if (req.user.mongouser.isVoted(doc._id)) {
+        await doc
+          .populate({
+            path: popOptions,
 						model: 'resources',
-						populate: {
-							path: 'images',
-							model: 'image',
-							select: 'name url',
-							populate: {
-								path: 'votes',
-								model: 'Votes',
-								select: 'count  updatedAt',
-							},
-						},
-					})
-					.execPopulate();
-			} else {
-				await doc
-					.populate({
-						path: popOptions,
+						select: '-_id -__v',
+            populate: {
+              path: 'images',
+              model: 'image',
+              select: 'name url',
+              populate: {
+                path: 'votes',
+                model: 'Votes',
+                select: 'count  updatedAt'
+              }
+            }
+          })
+          .execPopulate();
+      } else {
+        await doc
+          .populate({
+            path: popOptions,
 						model: 'resources',
-						populate: {
-							path: 'images',
-							model: 'image',
-							select: 'name url',
-						},
-					})
-					.execPopulate();
-			}
-			res.status(200).json({
-				status: 'success',
-				data: doc.toJSONFor(req.user.mongouser),
-			});
-		});
-	},
-	update() {
-		return catchAsync(async (req, res, next) => {
-			const doc = await Post.findByIdAndUpdate(req.params.id, req.body, {
-				new: true,
-				runValidators: true,
-			});
+						select: '-_id -__v',
+            populate: {
+              path: 'images',
+              model: 'image',
+              select: 'name url'
+            }
+          })
+          .execPopulate();
+      }
+      let data = null;
+      if (votedImage) {
+        data = JSON.parse(JSON.stringify(doc.toJSONFor(req.user.mongouser)));
+        data.resources.images.forEach((image, index) => {
+          if (image._id.toString() === votedImage.image.toString()) {
+            data.resources.images[index].votedByUser = true;
+          } else {
+            data.resources.images[index].votedByUser = false;
+          }
+        });
+      } else {
+        data = doc.toJSONFor(req.user.mongouser);
+      }
+      res.status(200).json({
+        status: 'success',
+        data
+      });
+    });
+  },
+  update() {
+    return catchAsync(async (req, res, next) => {
+      const doc = await Post.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+      });
+      if (!doc) {
+        return next(new AppError('No document found with that ID', 404));
+      }
+      res.status(200).json({
+        status: 'success',
+        data: {
+          data: doc.toJSONFor(req.user.mongouser)
+        }
+      });
+    });
+  },
+  delete() {
+    return catchAsync(async (req, res, next) => {
+      const doc = await Post.deleteOne({ _id: req.params.id });
+      if (!doc.deletedCount)
+        return next(new AppError('cannot find doc with that id', 404));
+      res.status(204).send();
+    });
+  },
+  getAll(options) {
+    return catchAsync(async (req, res, next) => {
+      let data = Post.find();
 
-			if (!doc) {
-				return next(
-					new AppError('No document found with that ID', 404)
-				);
-			}
-			res.status(200).json({
-				status: 'success',
-				data: {
-					data: doc.toJSONFor(req.user.mongouser),
-				},
-			});
-		});
-	},
-	delete() {
-		return catchAsync(async (req, res, next) => {
-			const doc = await Post.deleteOne({ _id: req.params.id });
-			if (!doc.deletedCount)
-				return next(new AppError('cannot find doc with that id', 404));
-			res.status(204).send();
-		});
-	},
-	getAll(options) {
-		return catchAsync(async (req, res, next) => {
-			let data = Post.find();
-			if (options.getRecentFirst) {
-				data.sort('-createdAt');
-			}
-			if (options.populateResources) {
-				data.populate({
-					path: 'resources',
+      let userVotedImages = await Votes.find({
+        voters: { $in: req.user.mongouser._id }
+      })
+        .select('image')
+        .exec();
+
+      if (options.getRecentFirst) {
+        data.sort('-createdAt');
+      }
+      if (options.populateResources) {
+        data.populate({
+          path: 'resources',
 					model: 'resources',
-					populate: {
-						path: 'images',
-						model: 'image',
-						select: 'name url',
-					},
-				});
-			}
-			if (options.populateAuthor) {
-				data.populate('author', 'name email');
-			}
-			data = await data;
-
-			if (!data) {
-				return next(new AppError('No Polls found with that ID', 404));
-			} else {
-				const dataWithVotes = await Promise.all(
-					data.map(async (post) => {
-						if (req.user.mongouser.isVoted(post._id)) {
-							await post
-								.populate({
-									path: 'resources',
+					select: '-_id -__v',
+          populate: {
+            path: 'images',
+            model: 'image',
+            select: 'name url'
+          }
+        });
+      }
+      if (options.populateAuthor) {
+        data.populate('author', 'name email');
+      }
+      data = await data;
+      if (!data) {
+        return next(new AppError('No Polls found with that ID', 404));
+      } else {
+        let dataWithVotes = await Promise.all(
+          data.map(async post => {
+            if (req.user.mongouser.isVoted(post._id)) {
+              await post
+                .populate({
+                  path: 'resources',
 									model: 'resources',
-									populate: {
-										path: 'images',
-										model: 'image',
-										select: 'name url',
-										populate: {
-											path: 'votes',
-											model: 'Votes',
-											select: 'count image updatedAt',
-										},
-									},
-								})
-								.execPopulate();
-						} else {
-							await post
-								.populate({
-									path: 'resources',
+									select: '-_id -__v',
+                  populate: {
+                    path: 'images',
+                    model: 'image',
+                    select: 'name url',
+                    populate: {
+                      path: 'votes',
+                      model: 'Votes',
+                      select: 'count image updatedAt'
+                    }
+                  }
+                })
+                .execPopulate();
+            } else {
+              await post
+                .populate({
+                  path: 'resources',
 									model: 'resources',
-									populate: {
-										path: 'images',
-										model: 'image',
-										select: 'name url',
-									},
-								})
-								.execPopulate();
-						}
+									select: '-_id -__v',
+                  populate: {
+                    path: 'images',
+                    model: 'image',
+                    select: 'name url'
+                  }
+                })
+                .execPopulate();
+            }
 
-						const doc = post.toJSONFor(req.user.mongouser);
-						return doc;
-					})
-				);
-				res.status(200).json({ data: dataWithVotes });
-			}
-		});
-	},
+            const doc = post.toJSONFor(req.user.mongouser);
+            return doc;
+          })
+        );
+
+        dataWithVotes = dataWithVotes.map(post => {
+          post = JSON.parse(JSON.stringify(post));
+          post.resources.images.forEach((image, index) => {
+            if (
+              userVotedImages.find(
+                item => item.image.toString() === image._id.toString()
+              )
+            ) {
+              post.resources.images[index].votedByUser = true;
+            } else {
+              post.resources.images[index].votedByUser = false;
+            }
+          });
+          return post;
+        });
+
+        res.status(200).json({ data: dataWithVotes });
+      }
+    });
+  }
 };
