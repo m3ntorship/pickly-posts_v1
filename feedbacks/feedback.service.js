8c4820c67d05861483fdbd127d69aaabc9b51f66
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-11-03 16:38:48 +0200

    check  today feedbacks and can't make more than 5 feedbacks daily

--- feedbacks/feedback.service.js
+++ feedbacks/feedback.service.js
@@ -7,10 +7,13 @@ var ajv = new Ajv({ allErrors: true });
 exports.feedbackService = {
   create() {
     return catchAsync(async (req, res) => {
+      const user = req.user.mongouser;
       const data = {
         category: req.body.category,
-        body: req.body.body
+        body: req.body.body,
+        author: user._id
       };
+
       var validate = ajv.compile(schema);
       var valid = validate(data);
       if (!valid) {
@@ -19,9 +22,28 @@ exports.feedbackService = {
         return;
       }
 
+      var now = new Date();
+      var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
+      let todayFeedbacks = await (
+        await Feedback.find({
+          author: user._id,
+          createdAt: { $gte: today }
+        })
+      ).length;
+      // console.log(feedbacksCount);
+      if (todayFeedbacks >= 5) {
+        res.status(429).json({
+          status: 'fail',
+          data: 'Many feedbacks today !'
+        });
+        return;
+      }
+
       const result = await Feedback.create({
         category: req.body.category,
-        body: req.body.body
+        body: req.body.body,
+        author: user._id
       });
       res.status(201).json({ status: 'success', data: result });
     });
