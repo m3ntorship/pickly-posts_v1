e8e1805fcf0abdeb5fabbd2e25b8fcd4b02f7002
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-11-03 16:09:29 +0200

    add feedback author property to authorize feedbacks

--- feedbacks/feedback.model.js
+++ feedbacks/feedback.model.js
@@ -1,19 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const feedbackSchema = new Schema(
   {
     category: {
       type: String,
-      required: true,
+      required: true
     },
     body: {
       type: String,
-      required: true,
-    }
+      required: true
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'user' }
   },
-    { timestamps: true }
+  { timestamps: true }
 );
 
-const Feedback = mongoose.model("Feedback", feedbackSchema);
+const Feedback = mongoose.model('Feedback', feedbackSchema);
 module.exports = Feedback;
