c3453ed0dfa6d369fee2d9d7c3cf75ee35e2a443
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-11-04 11:09:45 +0200

    route get categories

--- feedbacks/feedback.routes.js
+++ feedbacks/feedback.routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const feedbackContorller = require('./feedback.controller');
 const router = express.Router();
 
+router
+  .route('/')
+  .post(feedbackContorller.postFeedbacks)
+  .get(feedbackContorller.getAllFeedbacks);
 
-router.route('/')
-  .post( feedbackContorller.postFeedbacks)
-  .get( feedbackContorller.getAllFeedbacks);
+router.route('/categories').get(feedbackContorller.getAllCategories);
 
 module.exports = router;
-
