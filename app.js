e0b6f20c0a427b5f8edba7fead6f271a3993b734
Abud Zohair <abud.zohair@gmail.com>
2020-11-04 20:03:39 +0200

    add swaggerUI to Server

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const postRouter = require('./posts/post.routes');
 const imageRouter = require('./images/image.routes');
 const { protector } = require('./auth/auth.controller');
 const errorHandler = require('./middleware/errorhandler');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./m3ntorship-Posts-1.0.0-swagger.json');
+
 const logger = require('./util/logger');
 dotenv.config({ path: resolve('secrets', '.env') });
 
@@ -27,6 +30,7 @@ app.use(
 app.get('/health', (req, res) => {
   res.json({ ok: true });
 });
+app.get('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // protect all routes
 app.use(protector);
