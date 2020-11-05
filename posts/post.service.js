e12be02f7a2b4fdf887b7f0e942ec292e63fe5ed
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-11-05 16:54:20 +0200

    added limit and pagination to queries - sorted the posts by latest

--- posts/post.service.js
+++ posts/post.service.js
@@ -121,7 +121,11 @@ exports.postService = {
   getAll() {
     return catchAsync(async (req, res, next) => {
       const user = req.user.mongouser;
-      let posts = await getPopulatedPosts();
+      const { limit, page } = req.query;
+      let posts = await getPopulatedPosts()
+        .sort('-createdAt')
+        .limit(+limit)
+        .skip(+limit * (+page - 1));
       posts = await Promise.all(
         posts.map(async post => {
           post = await isVotedByCurrUser(user._id, post);
