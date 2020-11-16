2551feb1ce2276d32c39e3532e8a3e1892f62162
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-11-16 22:15:13 +0200

    fixed env variable name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const connect = require("./lib/db");
 const app = require("./app");
 
 const dbUrl =
-  process.env.DB_UI || "mongodb://localhost:27017/multer-m3ntorship";
+  process.env.DB_URI || "mongodb://localhost:27017/multer-m3ntorship";
 
 const port = 3001;
 connect(dbUrl)
