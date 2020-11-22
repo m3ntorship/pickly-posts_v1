215fa05b6fea5127043e3d08174859f67ce7d37d
seifeldeen3388 <seifeldeen3388@gmail.com>
2020-11-22 15:04:47 +0200

    added route for user own profile - refactored user service to change services from request handlers to functions

--- users/user.controller.js
+++ users/user.controller.js
@@ -1,4 +1,22 @@
+const { resolve } = require('path');
 const { userService } = require('./user.service');
+const serviceAccount = resolve('secrets', 'service-account.json');
+const catchAsync = require('../util/catchAsync');
+const AppError = require('../util/appError');
 
-exports.getUserPosts = userService.getUserPosts();
-exports.protector = userService.protector();
+exports.protector = userService.protector(serviceAccount);
+
+exports.getUserPosts = catchAsync(async (req, res) => {
+  const userPosts = await userService.getUserPosts(
+    req.params.userId.toString(),
+    req.user.mongouser._id.toString()
+  );
+  return res.status(200).json({ status: 'success', data: userPosts });
+});
+
+exports.getUserOwnPosts = catchAsync(async (req, res) => {
+  const userPosts = await userService.getUserPosts(
+    req.user.mongouser._id.toString()
+  );
+  return res.status(200).json({ status: 'success', data: userPosts });
+});
--- users/user.service.js
+++ users/user.service.js
@@ -1,10 +1,7 @@
-const { resolve } = require('path');
 const { protector } = require('@m3ntorship/pickly-protector');
 const User = require('./user.model');
 const Post = require('../posts/post.model');
 const catchAsync = require('../util/catchAsync');
-
-const serviceAccount = resolve('secrets', 'service-account.json');
 
 const userEnricher = async user => {
   let mongoUser = await User.findOne({ email: user.tokeninfo.email });
@@ -39,23 +36,24 @@ const getPosts = opts => {
 };
 
 exports.userService = {
-  protector() {
+  protector(serviceAccount) {
     return catchAsync(protector(serviceAccount, userEnricher));
   },
-  getUserPosts() {
-    return catchAsync(async (req, res, next) => {
-      let userPosts;
-      if (req.user.mongouser._id.toString() === req.params.userId) {
-        userPosts = await getPosts({
-          author: req.params.userId
-        });
-      } else {
-        userPosts = await getPosts({
-          author: req.params.userId,
-          isAnonymous: false
-        });
-      }
-      return res.status(200).json({ status: 'success', data: userPosts });
+  getUserPosts(profileOwnerId, userId) {
+    if (!userId) {
+      //   /users/posts endpoint
+      return getPosts({
+        author: profileOwnerId
+      });
+    }
+    if (userId === profileOwnerId) {
+      return getPosts({
+        author: profileOwnerId
+      });
+    }
+    return getPosts({
+      author: profileOwnerId,
+      isAnonymous: false
     });
   }
 };
